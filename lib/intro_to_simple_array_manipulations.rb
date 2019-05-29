bb33929ad3236d39eedc2fc6c4e71b26424305bd
beatific-revision-5216 <ivanov_chris@yahoo.com>
2019-05-29 03:56:45 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,48 @@
+
+def using_push(a, s)
+  a.push(s)
+end
+
+def using_unshift(a, s)
+  a.unshift(s)
+end
+
+def using_pop(a)
+  a.pop
+end
+
+def pop_with_args(a)
+  a.pop 2
+end
+
+def using_shift(a)
+  a.shift
+end
+
+def shift_with_args(a)
+  a.shift 2
+end
+
+def using_concat(a, b)
+  a.concat(b)
+end
+
+def using_insert(a, s)
+  a.insert(4, s)
+end
+
+def using_uniq(a)
+  a.uniq
+end
+
+def using_flatten(a)
+  a.flatten
+end
+
+def using_delete(a, s)
+  a.delete s
+end
+
+def using_delete_at(a, s)
+  a.delete_at s
+end
