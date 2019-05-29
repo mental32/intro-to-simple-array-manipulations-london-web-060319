325f99cb14200ab1cfa7d0fa5afe2626d8e15baf
beatific-revision-5216 <ivanov_chris@yahoo.com>
2019-05-29 03:54:04 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -16,22 +16,11 @@ def pop_with_args(a)
 end
 
 def using_shift(a)
-  a.shift(a)
+  a.shift a
 end
 
-describe "#shift_with_args" do 
-  before(:each) do 
-    @ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
-    @brands_removed = shift_with_args(@ice_cream_brands)
-  end
-  
-  it "takes in an argument of an array and uses the shift method with an argument of 2 to remove and return the first 2 items from the array" do 
-    expect(@brands_removed).to eq(["Blue Bell Creameries", "Ben & Jerry's"])
-  end
-
-  it "decreases the length of the array by 2" do 
-    expect(@brands_removed.size).to eq(2)
-  end
+def shift_with_args(a)
+  a.shift 2
 end
 
 describe "#using_concat" do 
