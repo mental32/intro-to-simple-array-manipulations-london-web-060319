c29377b1d25980f6e8759a9e7000937d2b84fb16
beatific-revision-5216 <ivanov_chris@yahoo.com>
2019-05-29 03:56:06 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -35,27 +35,14 @@ def using_uniq(a)
   a.uniq
 end
 
-describe "#using_flatten" do 
-  it "takes in an argument of an array that contains other arrays and uses the flatten method to return an array of strings" do 
-     private_colleges_in_newyork = ["New York University", ["Manhattan School of Music ", "Columbia University"], "The Juilliard School", "Bard College", "Cooper Union"]
-     flat_array = using_flatten(private_colleges_in_newyork)
-     expect(flat_array).to eq(["New York University", "Manhattan School of Music ", "Columbia University", "The Juilliard School", "Bard College", "Cooper Union"])
-  end
+def using_flatten(a)
+  a.flatten
 end
 
-
-describe "#using_delete" do 
-  it "takes in two arguments, an array and a string, and uses the delete method to remove any items from the array that are equal to that string" do
-    instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
-    no_offense_steven = using_delete(instructors, "Steven")
-    expect(instructors).not_to include("Steven")
-  end
+def using_delete(a, s)
+  a.delete s
 end
 
-describe "#using_delete_at" do 
-  it "takes in two arguments, an array and an integer and deletes the element at the index of the array that is equal to that integer" do 
-    famous_robots = ["the dog from doctor who", "R2D2", "Ultron"]
-    deleted_robot = using_delete_at(famous_robots, 2)
-    expect(deleted_robot).to eq("Ultron")
-  end
+def using_delete_at(a, s)
+  a.delete_at s
 end
